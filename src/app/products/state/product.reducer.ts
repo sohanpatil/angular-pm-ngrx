4630ad146907894df965ca977de3ec0687459fed
Patil, Sohan <Patil.Sohan@principal.com>
2019-02-28 19:34:18 +0530

    Changes made on 02/28/2019

--- src/app/products/state/product.reducer.ts
+++ src/app/products/state/product.reducer.ts
@@ -1,4 +1,24 @@
-export function reducer(state, action){
+import { Product } from "../product";
+import * as fromRoot from '../../state/app.state';
+
+export interface State extends fromRoot.State {
+    products : ProductState;
+}
+
+export interface ProductState{
+    showProductCode:boolean;
+    currentProduct: Product;
+    products: Product[]
+}
+
+const initialProductState:ProductState  ={
+    currentProduct : null,
+    products : [],
+    showProductCode : true
+    
+}
+
+export function reducer(state: ProductState = initialProductState, action): ProductState{
     switch(action.type){
 
         case 'TOGGLE_PRODUCT_CODE':
